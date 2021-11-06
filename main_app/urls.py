f5ecf6b9b51411ba55422bc375175145168939ef
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-11-05 17:06:44 -0700

    Add class based view on Tasks, SignUp, Finalize navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tasks/', views.tasks_index, name='tasks_index'),
+    path('tasks/create/', views.TasksCreate.as_view(), name='tasks_create'),
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Task
 
 # Create your views here.
 
 def home(request):
   return render(request, 'home.html')
+
+def tasks_index(request):
+  return render(request, 'tasks/tasks_index.html') 
+
+class TasksCreate(CreateView):
+  model = Task
+  fields = ['title', 'desc', 'endDate', 'createDate', 'bid', 'budget', 'category']
+
