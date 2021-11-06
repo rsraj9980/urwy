167a844f54bd8d0c0a8872d7b133e7f80f240311
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-11-05 17:07:11 -0700

    Finalize previous commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', views.tasks_index, name='tasks_index'),
     path('tasks/create/', views.TasksCreate.as_view(), name='tasks_create'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from .models import Task
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -14,3 +16,21 @@ class TasksCreate(CreateView):
   model = Task
   fields = ['title', 'desc', 'endDate', 'createDate', 'bid', 'budget', 'category']
 
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('tasks_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
