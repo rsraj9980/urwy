b884663acc563196661dede78e4ee8965c5298e6
rsraj9980 <91348564+rsraj9980@users.noreply.github.com>
2021-11-08 18:54:42 -0500

    Add My Tasks page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', views.tasks_index, name='index'),
+    path('tasks/mytasks/', views.my_tasks, name='my_tasks'),
     path('tasks/<int:task_id>/', views.tasks_detail, name='detail'),
     path('tasks/create/', views.TasksCreate.as_view(), name='tasks_create'),
     path('tasks/<int:pk>/update/', views.TasksUpdate.as_view(), name='tasks_update'),
