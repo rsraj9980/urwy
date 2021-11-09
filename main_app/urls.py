48bb9c7fdc283a4dfe68f2086bcea2a1c80b12d4
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-11-09 10:56:41 -0800

    Add Bidding UI and database updating

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('tasks/mytasks/', views.my_tasks, name='my_tasks'),
     path('tasks/<int:task_id>/', views.tasks_detail, name='detail'),
     path('tasks/<int:task_id>/bid/', views.bids_detail, name='bids_detail'),
+    path('tasks/<int:task_id>/add_bid/', views.add_bid, name='add_bid'),
     path('tasks/create/', views.TasksCreate.as_view(), name='tasks_create'),
     path('tasks/<int:pk>/update/', views.TasksUpdate.as_view(), name='tasks_update'),
     path('tasks/<int:pk>/delete/', views.TasksDelete.as_view(), name='tasks_delete'),
     path('accounts/signup/', views.signup, name='signup'),
+
 ]
 
